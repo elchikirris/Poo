84f7fbc5a6db1bd26c3ec8a54d8886f7b5b541b6
elchikirris <4639531716edgar@gmail.com>
2023-09-11 08:36:33 -0600

    Practica 3

---
+++ _p51_SumaPromedio.java
@@ -0,0 +1,33 @@
+// Muestra los numero n de a serie de Fibinacci
+// Edgar Israel Nieves Bautista
+// 11 de septiembre del 2023
+import java.util.Scanner;
+
+public class _p51_SumaPromedio {
+    public static void main(String[] args) {
+        int num1 = 0, num2 = 1, num3 = 0, count = 0, n;
+        char resp;
+        Scanner obj = new Scanner(System.in);
+
+        do {
+            System.out.print("\033[H\033[2J"); System.out.flush();
+            System.out.print("Muestra los primeros n números de la serie de Fibonacci\n");
+
+            System.out.print("Ingresa el valor máximo > "); n = obj.nextInt();
+            
+            while (count < n) {
+                count ++;
+                System.out.printf("%d ", num3);
+                num3 = num1 + num2;
+                num1 = num2;
+                num2 = num3;
+            }
+
+            do {
+                System.out.printf("\nDeseas continuar? (S/N) > "); resp = Character.toUpperCase((obj.next().charAt(0)));
+            } while (!(resp == 'S' || resp == 'N'));
+
+        } while (resp != 'N');
+
+    }
+}
